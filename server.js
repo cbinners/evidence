36bc1e6e23b5de3324019b8edb3775d872c71770
csauve <connorsauve@gmail.com>
2013-12-07 13:43:46 -0800

    Added API routes connected to resources

--- server.js
+++ server.js
@@ -20,9 +20,26 @@ process.on("uncaughtException", function(error) {
 	console.log(error);
 });
 
-app.get(config.apiPrefix + "/example", function(req, res) {
-	res.send("Data from API");
-});
+var issueResource = require("./resources/issueResource");
+app.get(config.apiPrefix + "/issues", issueResource.list);
+app.get(config.apiPrefix + "/issues/:id", issueResource.get);
+app.post(config.apiPrefix + "/issues", issueResource.create);
+app.put(config.apiPrefix + "/issues/:id", issueResource.update);
+app.del(config.apiPrefix + "/issues/:id", issueResource.remove);
+
+var viewpointResource = require("./resources/viewpointResource");
+app.get(config.apiPrefix + "/viewpoints", viewpointResource.list);
+app.get(config.apiPrefix + "/viewpoints/:id", viewpointResource.get);
+app.post(config.apiPrefix + "/viewpoints", viewpointResource.create);
+app.put(config.apiPrefix + "/viewpoints/:id", viewpointResource.update);
+app.del(config.apiPrefix + "/viewpoints/:id", viewpointResource.remove);
+
+var cardResource = require("./resources/cardResource");
+app.get(config.apiPrefix + "/cards", cardResource.list);
+app.get(config.apiPrefix + "/cards/:id", cardResource.get);
+app.post(config.apiPrefix + "/cards", cardResource.create);
+app.put(config.apiPrefix + "/cards/:id", cardResource.update);
+app.del(config.apiPrefix + "/cards/:id", cardResource.remove);
 
 //all other routes serve the ember app
 app.all("*", function(req, res) {
