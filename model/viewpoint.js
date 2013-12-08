4d2d83618f7b12aaf1d98363e8347ebaaaf4a06a
csauve <connorsauve@gmail.com>
2013-12-07 23:23:03 -0800

    Issue page now loads cards for each view via a separate per-view controller

--- model/viewpoint.js
+++ model/viewpoint.js
@@ -5,7 +5,7 @@ var Viewpoint = new Schema({
 	title: {type: String, required: true, trim: true, max: 150},
 	brightCounter: {type: Number, required: true, default: 0},
 	//user: {type: String, required: true},
-	issueSlug: {type: String, unique: true, max: 100, trim: true, required: true, match: /^[\da-zA-Z-_]*$/},
+	issueSlug: {type: String, max: 100, trim: true, required: true, match: /^[\da-zA-Z-_]*$/},
 	datePosted: {type: Date, required: true, default: Date.now}
 });
 
--- web-app/static/js/angular-app.js
+++ web-app/static/js/angular-app.js
@@ -1,47 +1,48 @@
 var app = angular.module("evidence", ["ngRoute", "ngSanitize"]);
 
 app.config(function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $routeProvider
-    .when("/", {controller: HomeCtrl, templateUrl: "/static/templates/home.html"})
-    .when("/about", {templateUrl: "/static/templates/about.html"})
-    .when("/issue/:slug", {controller: IssueCtrl, templateUrl: "/static/templates/issue.html"})
-    .otherwise({redirectTo: "/"});
+	$locationProvider.html5Mode(true);
+	$routeProvider
+	.when("/", {controller: HomeCtrl, templateUrl: "/static/templates/home.html"})
+	.when("/about", {templateUrl: "/static/templates/about.html"})
+	.when("/issue/:slug", {controller: IssueCtrl, templateUrl: "/static/templates/issue.html"})
+	.otherwise({redirectTo: "/"});
 });
 
 function HomeCtrl($scope, $routeParams) {
-    $scope.doThing = function() {
-        $scope.data = "Data changed";
-    };
-    
-    $scope.data = "Some data";
+	$scope.doThing = function() {
+		$scope.data = "Data changed";
+	};
+	
+	$scope.data = "Some data";
 
-    $.get("/api/example", function(response) {
-    	$scope.$apply(function() {
-    		$scope.dataFromApi = null;
-    	});
-    });
+	$.get("/api/example", function(response) {
+		$scope.$apply(function() {
+			$scope.dataFromApi = null;
+		});
+	});
 }
 
 function IssueCtrl($scope, $routeParams) {
-    $.get("/api/issues?slug=" + $routeParams.slug, function(response) {
-        $scope.$apply(function() {
-            $scope.issue = response[0] || null;
+	//load the issue
+	$.get("/api/issues?slug=" + $routeParams.slug, function(response) {
+		$scope.$apply(function() {
+			$scope.issue = response[0] || null;
+		});
+	});
 
-            if ($scope.issue) {
-                $.get("/api/viewpoints", function(views) {
-                    $scope.$apply(function(){
-                        $scope.views = views || null;
-                        if ($scope.views) {
-                            $.get("/api/cards", function(cards){
-                                $scope.$apply(function(){
-                                    $scope.cards = cards;
-                                });
-                            });
-                        }
-                    });
-                });
-            }
-        });
-    });
+	//load viewpoints for the issue
+	$.get("/api/viewpoints?issueSlug=" + $routeParams.slug, function(views) {
+		$scope.$apply(function() {
+			$scope.views = views || null;
+		});
+	});
+}
+
+function CardsCtrl($scope) {
+	$.get("/api/cards?viewpointId=" + $scope.view._id, function(cards) {
+		$scope.$apply(function() {
+			$scope.cards = cards;
+		});
+	});
 }
