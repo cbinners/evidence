46388e167414825ef8abe4d059018f5a7d089510
csauve <connorsauve@gmail.com>
2013-12-09 02:37:00 -0800

    Added unique constraints to view and issue titles

--- model/issue.js
+++ model/issue.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var Issue = new Schema({
-	title: {type: String, required: true, trim: true, max: 150},
+	title: {type: String, unique: true, required: true, trim: true, max: 150},
 	brightCounter: {type: Number, required: true, default: 0},
 	//user: {type: String, required: true},
 	slug: {type: String, unique: true, max: 100, trim: true, required: true, match: /^[\da-zA-Z-_]*$/},
