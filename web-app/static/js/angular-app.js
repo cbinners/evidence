0f073f8604d1bd526c6e9024bbd92e7d637e17c7
csauve <connorsauve@gmail.com>
2013-12-09 00:55:13 -0800

    issues are now editable and can be created from the nav bar link

--- web-app/static/js/angular-app.js
+++ web-app/static/js/angular-app.js
@@ -5,15 +5,23 @@ app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 	.when("/", {controller: HomeCtrl, templateUrl: "/static/templates/home.html"})
 	.when("/about", {templateUrl: "/static/templates/about.html"})
+	.when("/issue/new", {controller: EditIssueCtrl, templateUrl: "/static/templates/editissue.html"})
+	.when("/issue/:slug/edit", {controller: EditIssueCtrl, templateUrl: "/static/templates/editissue.html"})
 	.when("/issue/:slug", {controller: IssueCtrl, templateUrl: "/static/templates/issue.html"})
 	.otherwise({templateUrl: "/static/templates/404.html"});
 });
 
 app.filter("fromNow", function() {
-    return function(dateString) {
-        return moment(new Date(dateString)).fromNow()
-    };
+	return function(dateString) {
+		return moment(new Date(dateString)).fromNow()
+	};
 });
+
+function GlobalCtrl($scope, $location) {
+	$scope.go = function (path) {
+		$location.path(path);
+	};
+}
 
 function HomeCtrl($scope, $routeParams) {
 	$.get("/api/issues", function(response) {
@@ -21,6 +29,51 @@ function HomeCtrl($scope, $routeParams) {
 			$scope.issues = response;
 		});
 	});
+}
+
+function EditIssueCtrl($scope, $routeParams, $location) {
+	if ($routeParams.slug) {
+		$.get("/api/issues?slug=" + $routeParams.slug, function(response) {
+			$scope.$apply(function() {
+				$scope.issue = response[0] || null;
+				$scope.loaded = true;
+			});
+		});
+	} else {
+		$scope.issue = {};
+		$scope.newIssue = true;
+	}
+
+	$scope.submit = function() {
+		var id = $scope.issue._id;
+		$.ajax({
+			url: id ? "/api/issues/" + id : "/api/issues",
+			type: id ? "put" : "post",
+			data: $scope.issue,
+			success: function(response) {
+				$scope.$apply(function() {
+					$location.path("/issue/" + response.slug);
+				});
+			},
+			error: function(response) {
+				$scope.$apply(function() {
+					$scope.errorMessage = "Validation failed";
+				});
+			}
+		});
+	};
+
+	$scope.del = function() {
+		$.ajax({
+			type: "delete",
+			url: "/api/issues/" + $scope.issue._id,
+			success: function(response) {
+				$scope.$apply(function() {
+					$location.path("/");
+				});
+			}
+		});
+	};
 }
 
 function IssueCtrl($scope, $routeParams) {
