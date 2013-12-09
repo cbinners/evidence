c7cd7f59f789269610fe147a2e85e718af7317c1
csauve <connorsauve@gmail.com>
2013-12-09 02:24:44 -0800

    Cards can be added to views now

--- model/card.js
+++ model/card.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var Card = new Schema({
 	viewpointId: {type: Schema.ObjectId, required: true},
 	title: {type: String, required: true, trim: true, max: 150},
+	url: {type: String, required: true, trim: true, max: 200},
 	brightCounter: {type: Number, required: true, default: 0},
 	thumbnailUrl: {type: String},
 	//user: {type: String, required: true}
--- web-app/static/js/angular-app.js
+++ web-app/static/js/angular-app.js
@@ -77,6 +77,27 @@ function EditIssueCtrl($scope, $routeParams, $location) {
 }
 
 function IssueCtrl($scope, $routeParams) {
+	//create a new card
+	$scope.submitCard = function() {
+		$scope.card_model.viewpointId = add_card_to_view_id;
+		$("#cardmodal").modal("hide");
+		$.ajax({
+			url: "/api/cards",
+			type: "post",
+			data: $scope.card_model,
+			success: function(response) {
+				$scope.$apply(function() {
+					debugger;
+					if (!add_card_to_view.cards) {
+						add_card_to_view.cards = [];
+					}
+					add_card_to_view.cards.push(response);
+					$scope.card_model = {};
+				});
+			}
+		});
+	};
+
 	//create a new viewpoint
 	$scope.submitView = function() {
 		$("#viewmodal").modal("hide");
@@ -87,6 +108,7 @@ function IssueCtrl($scope, $routeParams) {
 			data: $scope.view_model,
 			success: function(response) {
 				$scope.$apply(function() {
+					$scope.view_model = {};
 					response.visible = true;
 					$scope.views.push(response);
 				});
@@ -150,12 +172,11 @@ function IssueCtrl($scope, $routeParams) {
 }
 
 function ViewpointCtrl($scope) {
-	$.get("/api/cards?viewpointId=" + $scope.view._id, function(cards) {
-		$scope.$apply(function() {
-			$scope.cards = cards;
-		});
-	});
-
+	$scope.showCardModal = function(view) {
+		add_card_to_view = view;
+		add_card_to_view_id = view._id;
+		$("#cardmodal").modal("show");
+	};
 
 	$scope.brightView = function(view) {
 		view.brightCounter++;
@@ -184,4 +205,11 @@ function ViewpointCtrl($scope) {
 			}
 		});
 	};
+
+	$.get("/api/cards?viewpointId=" + $scope.view._id, function(cards) {
+		$scope.$apply(function() {
+			$scope.view.cards = cards;
+		});
+	});
+
 }
